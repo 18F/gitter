4ee37cf27576f1d5e83490754376cf39021898fe
V David Zvenyach <vladlen.zvenyach@gsa.gov>
2015-05-10 15:51:11 -0400

    MD5 and travis apparently do not get along

--- gitter.sh
+++ gitter.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 INPUT=${1-respondents.csv} # Get csv from command line or default to respondents.csv 
+BUCKET=${2-test}
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
 
 (tail -n +2 $INPUT ; echo )| while IFS=, read RESPONDENT REPOSITORY
@@ -13,7 +14,7 @@ do
 	git clone $REPOSITORY && tar -czf responses/$RESPONDENT.tar.gz $REPONAME && rm -rf $REPONAME
 
 	if [ $TEST ]; then
-		s3cmd --config tests/s3.conf put responses/$RESPONDENT.tar.gz s3://test
+		s3cmd --config tests/s3.conf --disable-multipart put responses/$RESPONDENT.tar.gz s3://$BUCKET
 	fi
 
 done
